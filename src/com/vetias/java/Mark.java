3d46e3fa979ecc5d7a274cfe36c0bafd961a3be6
sivanagalingam26 <sivanagalingamr24it@vetias.ac.in>
2025-07-02 14:58:57 +0530

    markcode

---
+++ src/com/vetias/java/Mark.java
@@ -0,0 +1,12 @@
+package com.vetias.java;
+
+public class Mark {
+
+    public static void main(String[] args){
+        int [] marks ={90,87,67,76,88};
+        for (int i = 0; i < marks.length; i++) {
+    System.out.println(marks[i]);
+}
+    }
+    
+}
