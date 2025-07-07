4853b61867c25604438d145a6024b91d1412e8f7
sivanagalingam26 <sivanagalingamr24it@vetias.ac.in>
2025-07-07 13:26:55 +0530

    Count

--- src/com/vetias/Temperature/bin/Wordcount.java
+++ src/com/vetias/Temperature/bin/Wordcount.java
@@ -3,7 +3,7 @@ package com.vetias.Temperature.bin;
 public class Wordcount {
 
     public static void main(String[] args) {
-        String sentance ="This program shows how can we split a string into multiple. we need to find the program";
+        String sentance ="This program shows how can we split a string into multiple .we need to find the program";
         String Words[]= sentance.split(" ");
         System.out.println(Words.length);
 
