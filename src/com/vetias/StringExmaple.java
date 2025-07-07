fdfdd4b5cd726f9005e26c7ccbd3430dd0073582
sivanagalingam26 <sivanagalingamr24it@vetias.ac.in>
2025-07-07 10:32:00 +0530

    Stringname

---
+++ src/com/vetias/StringExmaple.java
@@ -0,0 +1,14 @@
+package com.vetias;
+
+public class StringExmaple {
+
+    public static void main(String[] args) {
+        String firstname="SIVA";
+        String lastname="NAGALINGAM";
+
+        String fullname = firstname+"  "+lastname;
+
+        System.out.println(fullname.toLowerCase());       
+    }
+
+}
