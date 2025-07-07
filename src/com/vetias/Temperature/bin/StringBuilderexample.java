ec797e89f17baf65748996f166dd8dc3cbdefdce
sivanagalingam26 <sivanagalingamr24it@vetias.ac.in>
2025-07-07 14:26:18 +0530

    Builder

---
+++ src/com/vetias/Temperature/bin/StringBuilderexample.java
@@ -0,0 +1,19 @@
+package com.vetias.Temperature.bin;
+
+public class StringBuilderexample {
+    public static void main(String[] args) {
+        StringBuilder sb= new StringBuilder();
+        sb.append("java");
+        sb.append(" is");
+        sb.append(" the");
+        sb.append(" most");
+        sb.append(" used");
+        sb.append(" language");
+        sb.append(" for");
+        sb.append(" Enterprise");
+        sb.append(" Application");
+        System.out.println(sb.toString());
+        
+    }
+
+}
