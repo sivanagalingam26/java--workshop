a54d396f158bbfa624b3e1908f5ececf12eb3e8a
sivanagalingam26 <sivanagalingamr24it@vetias.ac.in>
2025-07-07 13:23:45 +0530

    Words

--- src/com/vetias/StringExmaple.java
+++ src/com/vetias/StringExmaple.java
@@ -3,21 +3,22 @@ package com.vetias;
 public class StringExmaple {
 
     public static void main(String[] args) {
-         String firstname="SIVA";
-         String lastname="NAGALINGAM";
+        //  String firstname="SIVA";
+        //  String lastname="NAGALINGAM";
 
-         String fullname = firstname+"  "+lastname;
+        //  String fullname = firstname+"  "+lastname;
 
-         System.out.println(fullname.toLowerCase());   
+        //  System.out.println(fullname.toLowerCase());   
         
-        String sentance ="This program  shows how can we split a string into multiple";
-        String words[] = sentance.split(" ");
-        
-        for (String word : words){
-           String upperCaseString  = word.toUpperCase();
-            System.out.println(upperCaseString);
+        String sentance ="This program shows how can we split a string into multiple. we need to find the program";
+        String Words[]= sentance.split(" ");
+        System.out.println(Words.length);
+        String sen[]=sentance.split(" ");
+        System.out.println(sen.length);
+        String dot[]=sentance.split(" \\");
+        System.out.println(dot.length);
+
         }
     }
 
 
-}
