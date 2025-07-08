ec6b5634c33d7d94964cb10f52bcf012d6443db0
sivanagalingam26 <sivanagalingamr24it@vetias.ac.in>
2025-07-08 10:55:12 +0530

    Registration

---
+++ src/com/vetias/Temperature/bin/RegistrationTimeLeft.java
@@ -0,0 +1,16 @@
+package com.vetias.Temperature.bin;
+
+import java.time.LocalTime;
+
+public class RegistrationTimeLeft {
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        LocalTime registrationEndTime = LocalTime.of(18,00,00);
+        int hourLeft = registrationEndTime.getHour()-currentTime.getHour();
+        int mimuteLeft = registrationEndTime.getMinute()-currentTime.getMinute();
+        int secondLeft = registrationEndTime.getSecond()-currentTime.getSecond();
+        System.out.println("you have"+ hourLeft+ "hours,"+ mimuteLeft+ "mimute,"+ secondLeft+ "second");           
+
+    }
+
+}
