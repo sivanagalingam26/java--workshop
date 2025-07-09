def001b343f7e8a7f9888c884a588f1f790c569d
sivanagalingam26 <sivanagalingamr24it@vetias.ac.in>
2025-07-09 10:12:01 +0530

    Error

---
+++ src/com/vetias/Temperature/bin/CharacterBasereader.java
@@ -0,0 +1,22 @@
+package com.vetias.Temperature.bin;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CharacterBasereader {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("README.nd"))) {
+    String line;
+    while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+    }
+}catch (IOException e){
+    System.err.println("An error occured in reading the file");
+    e.printStackTrace();
+
+}
+    }
+
+
+}
---
+++ src/com/vetias/Temperature/bin/FileHanding.java
@@ -0,0 +1,16 @@
+package com.vetias.Temperature.bin;
+
+public class FileHanding {
+    public static void main(String[] args) {
+        
+        if(file.isDirectory()){
+            System.out.println("It is a directory:");
+            File[] file = file.listfile();
+            if(file != null) {
+                System.out.println("Flie is the directory:");
+
+            }
+        }
+    }
+
+}
